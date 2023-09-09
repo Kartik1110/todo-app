9f1c131c5d7fbd8a6a62224faafe21d0990706cd
Kartik Pal <kartik.p1110@gmail.com>
2023-09-09 15:11:22 +0530

    feat: added tailwind, changed UI

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,37 +1,83 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import { useState } from "react";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = async () => {
-        const response = await fetch('http://localhost:3000/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
-        });
-        // Todo: Create a type for the response that you get back from the server
-        const data = await response.json();
-        if (data.token) {
-            localStorage.setItem("token", data.token)
-            window.location = "/todos";
-        } else {
-            alert("invalid credentials");
-        }
-    };
+  const handleLogin = async () => {
+    const response = await fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    // Todo: Create a type for the response that you get back from the server
+    const data = await response.json();
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      window.location = "/todos";
+    } else {
+      alert("invalid credentials");
+    }
+  };
 
-    return (
-        <div style={{justifyContent: "center", display: "flex", width: "100%"}}>
-            <div>
-                <h2>Login</h2>
-                <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
-                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
-                New here? <Link to="/signup">Signup</Link>
-                <button onClick={handleLogin}>Login</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="bg-red-300 h-screen flex justify-center items-center">
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="******************"
+            />
+            {/* <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p> */}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={handleLogin}
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/Components/TodoList.tsx
+++ client/src/Components/TodoList.tsx
@@ -1,85 +1,149 @@
-import { useState, useEffect } from 'react';
-import { authState } from '../store/authState.js';
-import {useRecoilValue} from "recoil";
+import { useState, useEffect } from "react";
+import { authState } from "../store/authState.js";
+import { useRecoilValue } from "recoil";
 
 interface Todo {
-    _id: string;
-    title: string;
-    description: string;
-    done: boolean;
+  _id: string;
+  title: string;
+  description: string;
+  done: boolean;
 }
 
 type TodoArray = Todo[];
 
 const TodoList = () => {
-    const [todos, setTodos] = useState<TodoArray>([]);
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const authStateValue = useRecoilValue(authState);
+  const [todos, setTodos] = useState<TodoArray>([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const authStateValue = useRecoilValue(authState);
 
-    useEffect(() => {
-        const getTodos = async () => {
-            const response = await fetch('http://localhost:3000/todo/todos', {
-                headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-            });
-            // Todo: Create a type for the response that you get back from the server
-            const data: Todo[] = await response.json();
-            setTodos(data);
-        };
-        getTodos();
-    }, []);
-
-    const addTodo = async () => {
-        const response = await fetch('http://localhost:3000/todo/todos', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${localStorage.getItem("token")}` },
-            body: JSON.stringify({ title, description })
-        });
-        const data = await response.json();
-
-        const newTodos = [];
-        for (let i = 0; i<todos.length; i++) {
-            newTodos.push(todos[i]);
-        }
-        
-        newTodos.push(data);
-        setTodos(newTodos);
+  useEffect(() => {
+    const getTodos = async () => {
+      const response = await fetch("http://localhost:3000/todo/todos", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      });
+      // Todo: Create a type for the response that you get back from the server
+      const data: Todo[] = await response.json();
+      setTodos(data);
     };
+    getTodos();
+  }, []);
 
-    const markDone = async (id) => {
-        const response = await fetch(`http://localhost:3000/todo/todos/${id}/done`, {
-            method: 'PATCH',
-            headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-        });
-        const updatedTodo = await response.json();
-        setTodos(todos.map((todo) => (todo._id === updatedTodo._id ? updatedTodo : todo)));
-    };
+  const addTodo = async () => {
+    const response = await fetch("http://localhost:3000/todo/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({ title, description }),
+    });
+    const data = await response.json();
 
-    return (
-        <div>
-            <div style={{display: "flex"}}>
-                <h2>Welcome {authStateValue.username}</h2>
-                <div style={{marginTop: 25, marginLeft: 20}}>
-                    <button onClick={() => {
-                        localStorage.removeItem("token");
-                        window.location = "/login";
-                    }}>Logout</button>
-                </div>
-            </div>
-            <h2>Todo List</h2>
-            <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Title' />
-            <input type='text' value={description} onChange={(e) => setDescription(e.target.value)} placeholder='Description' />
-            <button onClick={addTodo}>Add Todo</button>
+    const newTodos = [];
+    for (let i = 0; i < todos.length; i++) {
+      newTodos.push(todos[i]);
+    }
 
-            {todos.map((todo) => (
-                <div key={todo._id}>
-                    <h3>{todo.title}</h3>
-                    <p>{todo.description}</p>
-                    <button onClick={() => markDone(todo._id)}>{todo.done ? 'Done' : 'Mark as Done'}</button>
-                </div>
-            ))}
-        </div>
+    newTodos.push(data);
+    setTodos(newTodos);
+  };
+
+  const markDone = async (id) => {
+    const response = await fetch(
+      `http://localhost:3000/todo/todos/${id}/done`,
+      {
+        method: "PATCH",
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      }
     );
+    const updatedTodo = await response.json();
+    setTodos(
+      todos.map((todo) => (todo._id === updatedTodo._id ? updatedTodo : todo))
+    );
+  };
+
+  return (
+    <div className="bg-red-300 h-screen flex flex-col justify-center items-center">
+      <div>
+        <h2>Welcome {authStateValue.username}</h2>
+        <div>
+          <button
+            onClick={() => {
+              localStorage.removeItem("token");
+              window.location = "/login";
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <h2>Todo List</h2>
+
+      <div className="w-full max-w-md">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="title"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Title"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="description"
+            >
+              Description
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description"
+            />
+            {/* <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p> */}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              onClick={addTodo}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Add Todo
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        {todos.map((todo) => (
+          <div key={todo._id} className="flex justify-between pt-6">
+            <div className="flex flex-col">
+              <h3 className={todo.done ? "text-gray-900 font-bold text-lg line-through" : "text-gray-900 font-bold text-lg"}>{todo.title}</h3>
+              {/* <p>{todo.description}</p> */}
+              <p className={todo.done ? "w-full text-gray-900 line-through" : "w-full text-gray-900"}>{todo.description}</p>
+            </div>
+            <button
+              onClick={() => markDone(todo._id)}
+              className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-800 border-green-800 hover:bg-green-800"
+            >
+              {todo.done ? "Done" : "Mark as Done"}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
